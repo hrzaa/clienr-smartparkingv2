bada929f4202720c8774bafb1e9889e221832d8e
hrzaa <harezayoan@gmail.com>
2024-08-20 16:08:31 +0700

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,13 +14,10 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await axios.post(
-        `${API_BASE_URL}user/login`,
-        {
-          username,
-          password,
-        }
-      );
+      const response = await axios.post(`${API_BASE_URL}users/login`, {
+        username,
+        password,
+      });
       const { data } = response.data;
 
       const isSecure = window.location.protocol === "http:";
@@ -38,85 +35,82 @@ function Login() {
           : error.message;
       setError(errorMessage);
     }
-  }
+  };
 
-   return (
-     <section class="bg-gray-50">
-       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-         <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ">
-           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-               Smart Parking System
-             </h1>
+  return (
+    <section class="bg-gray-50">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+              Smart Parking System
+            </h1>
 
-             <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
-               {error && (
-                 <div
-                   class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
-                   role="alert"
-                 >
-                   <p class="font-bold">{error}</p>
-                 </div>
-               )}
-               <div>
-                 <label
-                   htmlFor="username"
-                   className="block mb-2 text-sm font-medium text-gray-900 "
-                 >
-                   Username
-                 </label>
-                 <input
-                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                   id="username"
-                   type="text"
-                   placeholder="Username"
-                   value={username}
-                   onChange={(e) => setUsername(e.target.value)}
-                   required
-                 />
-               </div>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
+              {error && (
+                <div
+                  class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
+                  role="alert"
+                >
+                  <p class="font-bold">{error}</p>
+                </div>
+              )}
+              <div>
+                <label
+                  htmlFor="username"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Username
+                </label>
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  id="username"
+                  type="text"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </div>
 
-               <div>
-                 <label
-                   htmlFor="password"
-                   className="block mb-2 text-sm font-medium text-gray-900"
-                 >
-                   Password
-                 </label>
-                 <input
-                   id="password"
-                   type="password"
-                   placeholder="Password"
-                   value={password}
-                   onChange={(e) => setPassword(e.target.value)}
-                   required
-                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                 />
-               </div>
-               <button
-                 type="submit"
-                 class="w-full text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-               >
-                 Sign in
-               </button>
-               <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                 Don’t have an account yet?{" "}
-                 <Link
-                   to="/register"
-                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                 >
-                   Sign Up
-                 </Link>
-               </p>
-             </form>
-           </div>
-         </div>
-       </div>
-     </section>
-   );
-
-};
-
-
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Password
+                </label>
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                />
+              </div>
+              <button
+                type="submit"
+                class="w-full text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Sign in
+              </button>
+              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don’t have an account yet?{" "}
+                <Link
+                  to="/register"
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Sign Up
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
 
 export default Login;
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // export const API_BASE_URL = "https://harezayoankristianto.online/api/";
 // export const API_BASE_URL = "http://localhost:5000/api/";
 // export const API_BASE_URL = "http://103.155.246.50:9085/api/";
-export const API_BASE_URL = "https://resort-check-novels-moldova.trycloudflare.com/api/";
+// export const API_BASE_URL = "https://resort-check-novels-moldova.trycloudflare.com/api/";
+export const API_BASE_URL = "https://parkir.radityafs.id/api/";
 
